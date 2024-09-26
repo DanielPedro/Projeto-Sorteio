2d125852eb8e3409e346c9fbd157e81a47300a1e
DanielPedro <dj.dheniel@gmail.com>
2024-09-25 23:27:35 -0300

    Primeiro commit - Projeto Sorteio

---
+++ scripts.js
@@ -0,0 +1,11 @@
+buttonSorteio = document.querySelector('.sortear')
+
+function generateNumber() {
+   const min = Math.ceil(document.querySelector('.input-min').value)
+   const max = Math.floor(document.querySelector('.input-max').value)
+
+   const result = Math.floor(Math.random() * (max - min + 1)) + min;
+   alert(result)
+  
+}
+buttonSorteio.addEventListener('click', generateNumber)
